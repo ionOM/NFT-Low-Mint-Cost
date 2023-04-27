1a0815b53223a7e0a9d3e317cecc69176aafd84d
ionOM <ionblockchain@gmail.com>
2023-04-27 14:36:44 +0200

    contract creation

---
+++ contracts/Minions20.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Minions20 is ERC721, Ownable {
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+
+    Counters.Counter private supply;
+
+    string public uriPrefix = "";
+    string public uriSuffix = ".json";
+    string public hiddenMetadataUri;
+
+    uint256 public cost = 0.01 ether;
+    uint256 public maxSupply = 10000;
+    uint256 public maxMintAmountPerTx = 5;
+
+    bool public paused = true;
+    bool public revealed = false;
+
+
+    constructor() ERC721("NAME", "SYMBOL") {
+      setHiddenMetadataUri("ipfs://__CID__/hidden.json");
+  }
+
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    _;
+  }
+
+  function totalSupply() public view returns (uint256) {
+    return supply.current();
+}
