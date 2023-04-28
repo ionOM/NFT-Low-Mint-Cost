3db623e3694481e4a35df844e5867f472f4949ad
ionOM <ionblockchain@gmail.com>
2023-04-28 19:35:39 +0200

    new contract functions

--- contracts/Minions20.sol
+++ contracts/Minions20.sol
@@ -23,17 +23,121 @@ contract Minions20 is ERC721, Ownable {
     bool public paused = true;
     bool public revealed = false;
 
-
     constructor() ERC721("NAME", "SYMBOL") {
-      setHiddenMetadataUri("ipfs://__CID__/hidden.json");
-  }
+        setHiddenMetadataUri("ipfs://__CID__/hidden.json");
+    }
 
-  modifier mintCompliance(uint256 _mintAmount) {
-    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
-    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
-    _;
-  }
+    modifier mintCompliance(uint256 _mintAmount) {
+        require(
+            _mintAmount > 0 && _mintAmount <= maxMintAmountPerTx,
+            "Invalid mint amount!"
+        );
+        require(
+            supply.current() + _mintAmount <= maxSupply,
+            "Max supply exceeded!"
+        );
+        _;
+    }
 
-  function totalSupply() public view returns (uint256) {
-    return supply.current();
+    function totalSupply() public view returns (uint256) {
+        return supply.current();
+    }
+
+    function mint(
+        uint256 _mintAmount
+    ) public payable mintCompliance(_mintAmount) {
+        require(!paused, "The contract is paused!");
+        require(msg.value >= cost * _mintAmount, "Insufficient funds!");
+
+        _mintLoop(msg.sender, _mintAmount);
+    }
+
+    function mintForAddress(
+        uint256 _mintAmount,
+        address _receiver
+    ) public mintCompliance(_mintAmount) onlyOwner {
+        _mintLoop(_receiver, _mintAmount);
+    }
+
+    function walletOfOwner(
+        address _owner
+    ) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+        uint256 currentTokenId = 1;
+        uint256 ownedTokenIndex = 0;
+
+        while (
+            ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply
+        ) {
+            address currentTokenOwner = ownerOf(currentTokenId);
+
+            if (currentTokenOwner == _owner) {
+                ownedTokenIds[ownedTokenIndex] = currentTokenId;
+
+                ownedTokenIndex++;
+            }
+
+            currentTokenId++;
+        }
+
+        return ownedTokenIds;
+    }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view virtual override returns (string memory) {
+        require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        if (revealed == false) {
+            return hiddenMetadataUri;
+        }
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        _tokenId.toString(),
+                        uriSuffix
+                    )
+                )
+                : "";
+    }
+
+    function setRevealed(bool _state) public onlyOwner {
+        revealed = _state;
+    }
+
+    function setCost(uint256 _cost) public onlyOwner {
+        cost = _cost;
+    }
+
+    function setMaxMintAmountPerTx(
+        uint256 _maxMintAmountPerTx
+    ) public onlyOwner {
+        maxMintAmountPerTx = _maxMintAmountPerTx;
+    }
+
+    function setHiddenMetadataUri(
+        string memory _hiddenMetadataUri
+    ) public onlyOwner {
+        hiddenMetadataUri = _hiddenMetadataUri;
+    }
+
+    function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+        uriPrefix = _uriPrefix;
+    }
+
+    function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+        uriSuffix = _uriSuffix;
+    }
+
+    function setPaused(bool _state) public onlyOwner {
+        paused = _state;
+    }
 }
